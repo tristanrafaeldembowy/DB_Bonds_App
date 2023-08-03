d7c58719afd8cabdd4bd3f46b59de5f6a01f9f67
skrajput01 <skrajput01@hotmail.com>
2023-08-03 15:24:11 +0100

    Updated data sql to import CSV data. Also included the CSV file for reference

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -2,13 +2,33 @@ INSERT INTO dogs (dog_id, name, age) VALUES (1, 'Einstein', 3);
 INSERT INTO dogs (dog_id, name, age) VALUES (2, 'Kaya', 5);
 INSERT INTO dogs (dog_id, name, age) VALUES (3, 'Lassie', 7);
 
-# Book (book_id, book_name
 
 
+/* Book (book_id, book_name */
+INSERT INTO Book
+(book_name)
+SELECT book_name
+FROM CSVREAD('C:/work/DB_Bonds_App/db-bonds-data.csv');
+
+/* Book_users (book_id, user_id) */
 
 
-INSERT INTO books (book_id, book_name)
-VALUES
-    (),
-    (),
-    
+/* Users (user_id, email, password) */
+
+
+/* Security (security_id, isin, cusip, bond_currency, type, bond_maturity_date, bond_holder, status) */
+INSERT INTO Security
+(isin, cusip, bond_currency, type, bond_maturity_date, bond_holder, status)
+SELECT isin, cusip, bond_currency, type, bond_maturity_date, bond_holder, status
+FROM CSVREAD('C:/work/DB_Bonds_App/db-bonds-data.csv');
+
+/* Trades (trade_id, book_id, security_id, counter_party_id, trade_type, trade_settlement_date, trade_currency, trade_status, trade_date, unit_price, quantity, coupon_percent, face_value_mn) */
+INSERT INTO Trades
+(trade_type, trade_settlement_date, trade_currency, trade_status, trade_date, unit_price, quantity, coupon_percent, face_value_mn)
+FROM CSVREAD('C:/work/DB_Bonds_App/db-bonds-data.csv');
+
+/* Counter_party (counter_party_id, issuer_name) */
+INSERT INTO Counter_party
+(issuer_name)
+FROM CSVREAD('C:/work/DB_Bonds_App/db-bonds-data.csv');
+

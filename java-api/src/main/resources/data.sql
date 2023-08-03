5bdf2046f5074dddc5dab304dcab6606f1478ba1
skrajput01 <skrajput01@hotmail.com>
2023-08-03 13:19:28 +0100

    Updated Schema sql to include appropriate validation for fields

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -2,5 +2,13 @@ INSERT INTO dogs (dog_id, name, age) VALUES (1, 'Einstein', 3);
 INSERT INTO dogs (dog_id, name, age) VALUES (2, 'Kaya', 5);
 INSERT INTO dogs (dog_id, name, age) VALUES (3, 'Lassie', 7);
 
+# Book (book_id, book_name
 
-INSERT INTO books
+
+
+
+INSERT INTO books (book_id, book_name)
+VALUES
+    (),
+    (),
+    
--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -6,54 +6,56 @@ DROP TABLE IF EXISTS Counter_party;
 DROP TABLE IF EXISTS Security;
 
 
-CREATE TABLE Book (
+CREATE TABLE IF NOT EXISTS Book (
     book_id INT NOT NULL AUTO_INCREMENT,
     book_name VARCHAR(70) NOT NULL,
     PRIMARY KEY (book_id)
 );
-CREATE TABLE Book_users (
+CREATE TABLE IF NOT EXISTS  Book_users (
     book_id INT NOT NULL,
     user_id INT NOT NULL,
     PRIMARY KEY (book_id, user_id),
     FOREIGN KEY (book_id) REFERENCES Book (book_id),
     FOREIGN KEY (user_id) REFERENCES Users (user_id)
 );
-CREATE TABLE Users (
+CREATE TABLE IF NOT EXISTS  Users (
     user_id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(40) NOT NULL UNIQUE,
     password VARCHAR(30) NOT NULL,
     PRIMARY KEY (user_id)
 );
-CREATE TABLE Trade (
+CREATE TABLE IF NOT EXISTS  Trade (
     trade_id INT NOT NULL AUTO_INCREMENT,
     book_id INT NOT NULL,
     security_id INT NOT NULL,
     counter_party_id INT NOT NULL,
-    trade_type VARCHAR(20) NOT NULL,
+    trade_type ENUM('BUY', 'SELL') NOT NULL,
     trade_settlement_date DATE NOT NULL,
-    trade_currency VARCHAR(20) NOT NULL,
-    status VARCHAR(20) NOT NULL,
+    trade_currency ENUM('USD', 'GBP') NOT NULL,
+    trade_status ENUM('open', 'closed') NOT NULL,
     trade_date DATE NOT NULL,
-    unit_price DECIMAL(10,2) NOT NULL,
+    unit_price FLOAT NOT NULL,
     quantity INT NOT NULL,
-    coupon_percent DECIMAL(10,2) NOT NULL,
+    coupon_percent FLOAT NOT NULL,
     face_value_mn INT NOT NULL,
     PRIMARY KEY (trade_id),
     FOREIGN KEY (book_id) REFERENCES Book (book_id),
     FOREIGN KEY (security_id) REFERENCES Security (security_id),
     FOREIGN KEY (counter_party_id) REFERENCES Counter_party (counter_party_id)
 );
-CREATE TABLE Counter_party (
+CREATE TABLE IF NOT EXISTS  Counter_party (
     counter_party_id INT NOT NULL AUTO_INCREMENT,
     issuer_name VARCHAR(70) NOT NULL,
     PRIMARY KEY (counter_party_id)
 );
-CREATE TABLE Security (
+CREATE TABLE IF NOT EXISTS  Security (
     security_id INT NOT NULL AUTO_INCREMENT,
-    isin VARCHAR(50) NOT NULL UNIQUE,
-    bond_currency VARCHAR(10) NOT NULL,
-    type VARCHAR(50) NOT NULL,
+    isin VARCHAR(50),
+    cusip VARCHAR(50),
+    bond_currency ENUM('USD', 'GBP') NOT NULL,
+    type ENUM('CORP', 'GOVN', 'SOVN') NOT NULL,
     bond_maturity_date DATE NOT NULL,
     bond_holder VARCHAR(70) NOT NULL,
+    status ENUM('active', 'inactive') NOT NULL,
     PRIMARY KEY (security_id)
 );

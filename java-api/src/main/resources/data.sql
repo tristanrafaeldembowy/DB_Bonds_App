10545695b7919169e708565399c732c27febcede
Raghavendra Tikare <raghavendra.t995@gmail.com>
2023-08-04 12:05:17 +0100

    SQl added

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -1,3 +1,29 @@
-INSERT INTO dogs (dog_id, name, age) VALUES (1, 'Einstein', 3);
-INSERT INTO dogs (dog_id, name, age) VALUES (2, 'Kaya', 5);
-INSERT INTO dogs (dog_id, name, age) VALUES (3, 'Lassie', 7);
+/* Book (book_id, book_name */
+INSERT INTO Book
+(book_name)
+SELECT DISTINCT book_name
+FROM CSVREAD('C:/work/DB_Bonds_App/db-bonds-data.csv');
+----
+----/* Book_users (book_id, user_id) */
+----
+----
+----/* Users (user_id, email, password) */
+----
+----
+/* Security (security_id, isin, cusip, issuer_name, coupon_percent, bond_currency, type, face_value_mn, bond_maturity_date, bond_holder, status) */
+INSERT INTO Security
+(isin, cusip, issuer_name, coupon_percent, bond_currency, type, face_value_mn, bond_maturity_date, bond_holder, status)
+SELECT isin, cusip, issuer_name, coupon_percent, bond_currency, type, face_value_mn, bond_maturity_date, bond_holder, status
+FROM CSVREAD('C:/work/DB_Bonds_App/db-bonds-data.csv');
+
+--/* Trade (trade_id, book_id, security_id, counter_party_id, trade_type, trade_settlement_date, trade_currency, trade_status, trade_date, unit_price, quantity, coupon_percent, face_value_mn) */
+----INSERT INTO Trade
+----(trade_type, trade_settlement_date, trade_currency, trade_status, trade_date, unit_price, quantity, coupon_percent, face_value_mn)
+----SELECT trade_type, trade_settlement_date, trade_currency, trade_status, trade_date, unit_price, quantity, coupon_percent, face_value_mn
+----FROM CSVREAD('C:/work/DB_Bonds_App/db-bonds-data.csv');
+--
+/* Counter_party (counter_party_id, issuer_name) */
+INSERT INTO Counter_party
+(issuer_name)
+SELECT DISTINCT issuer_name
+FROM CSVREAD('C:/work/DB_Bonds_App/db-bonds-data.csv');
--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -1,8 +1,57 @@
-DROP TABLE IF EXISTS dogs;
 
-CREATE TABLE dogs (
-    dog_id INT NOT NULL,
-    name VARCHAR(250) NOT NULL,
-    age INT NOT NULL
+CREATE TABLE IF NOT EXISTS Book (
+    book_id INT NOT NULL AUTO_INCREMENT,
+    book_name VARCHAR(70) NOT NULL,
+    PRIMARY KEY (book_id)
 );
 
+CREATE TABLE IF NOT EXISTS  Users (
+    user_id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(40) NOT NULL UNIQUE,
+--    password VARCHAR(30) NOT NULL,
+    PRIMARY KEY (user_id)
+);
+CREATE TABLE IF NOT EXISTS  Book_users (
+    book_id INT NOT NULL,
+    user_id INT NOT NULL,
+    PRIMARY KEY (book_id, user_id),
+    FOREIGN KEY (book_id) REFERENCES Book (book_id),
+    FOREIGN KEY (user_id) REFERENCES Users (user_id)
+);
+
+CREATE TABLE IF NOT EXISTS  Counter_party (
+    counter_party_id INT NOT NULL AUTO_INCREMENT,
+    issuer_name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (counter_party_id)
+);
+CREATE TABLE IF NOT EXISTS  Security (
+    security_id INT NOT NULL AUTO_INCREMENT,
+    isin VARCHAR(50),
+    cusip VARCHAR(50),
+    issuer_name varchar(100) NOT NULL,
+    coupon_percent FLOAT NOT NULL,
+    bond_currency ENUM('USD', 'GBP') NOT NULL,
+    type ENUM('CORP', 'GOVN', 'SOVN') NOT NULL,
+    face_value_mn float NOT NULL,
+    bond_maturity_date VARCHAR(20) NOT NULL,
+    bond_holder VARCHAR(70) NOT NULL,
+    status ENUM('active', 'inactive') NOT NULL,
+    PRIMARY KEY (security_id)
+);
+CREATE TABLE IF NOT EXISTS  Trade (
+    trade_id INT NOT NULL AUTO_INCREMENT,
+    book_id INT NOT NULL,
+    security_id INT NOT NULL,
+    counter_party_id INT NOT NULL,
+    trade_type ENUM('BUY', 'SELL') NOT NULL,
+    trade_settlement_date DATE NOT NULL,
+    trade_currency ENUM('USD', 'GBP') NOT NULL,
+    trade_status ENUM('open', 'closed') NOT NULL,
+    trade_date DATE NOT NULL,
+    unit_price FLOAT NOT NULL,
+    quantity INT NOT NULL,
+    PRIMARY KEY (trade_id),
+    FOREIGN KEY (book_id) REFERENCES Book (book_id),
+    FOREIGN KEY (security_id) REFERENCES Security (security_id),
+    FOREIGN KEY (counter_party_id) REFERENCES Counter_party (counter_party_id)
+);
